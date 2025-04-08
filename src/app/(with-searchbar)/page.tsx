4608dbdda65775853a520ba56946ccde3ad77bc8
baekjisu-dev <jisu.dev6476@gmail.com>
2025-04-08 22:19:38 +0900

    feat: 데이터 페칭 적용

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -5,7 +5,45 @@ import MovieItem from "@/components/movie-item";
 import style from "./page.module.css";
 
 // data
-import movies from "@/mock/dummy.json";
+import { MovieData } from "@/type";
+
+async function AllMovies() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie`,
+    {
+      next: {
+        revalidate: 60 * 60 * 24,
+      },
+    }
+  );
+
+  if (!response.ok) {
+    return <div>오류가 발생했습니다. 잠시 후 다시 시도해 주세요.</div>;
+  }
+
+  const allMovies: MovieData[] = await response.json();
+
+  return allMovies.map((movie) => <MovieItem key={movie.id} {...movie} />);
+}
+
+async function RecoMovies() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/random`,
+    {
+      next: {
+        revalidate: 60 * 60 * 24,
+      },
+    }
+  );
+
+  if (!response.ok) {
+    return <div>오류가 발생했습니다. 잠시 후 다시 시도해 주세요.</div>;
+  }
+
+  const recoMoives: MovieData[] = await response.json();
+
+  return recoMoives.map((movie) => <MovieItem key={movie.id} {...movie} />);
+}
 
 export default function Home() {
   return (
@@ -13,17 +51,13 @@ export default function Home() {
       <section>
         <h3>지금 가장 추천하는 영화 🍿</h3>
         <div className={style.reco_movie_area}>
-          {movies.slice(0, 3).map((movie) => (
-            <MovieItem key={movie.id} {...movie} />
-          ))}
+          <RecoMovies />
         </div>
       </section>
       <section>
         <h3>등록된 모든 영화 🎞️</h3>
         <div className={style.all_movie_area}>
-          {movies.map((movie) => (
-            <MovieItem key={movie.id} {...movie} />
-          ))}
+          <AllMovies />
         </div>
       </section>
     </div>
--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -5,9 +5,25 @@ import MovieItem from "@/components/movie-item";
 import style from "./page.module.css";
 
 // data
-import movies from "@/mock/dummy.json";
+import { MovieData } from "@/type";
 
-export default async function page() {
+export default async function page({
+  searchParams,
+}: {
+  searchParams: Promise<{ q: string }>;
+}) {
+  const { q } = await searchParams;
+
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/search?q=${q}`
+  );
+
+  if (!response.ok) {
+    return <div>오류가 발생했습니다. 잠시 후 다시 시도해 주세요.</div>;
+  }
+
+  const movies: MovieData[] = await response.json();
+
   return (
     <div className={style.container}>
       {movies.map((movie) => (
